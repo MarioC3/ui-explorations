2235b67e9b86677dcf103961a010231dd8cfcba7
Beto Carlos <me@beto.codes>
2024-12-02 17:26:23 -0700

    Adds explorations to tailwind and fontsize

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,7 @@ import Forms from '@tailwindcss/forms'
 export default {
 	content: [
 		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/explorations/**/*.{js,ts,jsx,tsx,mdx}",
 		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
 	],
 	theme: {
@@ -14,6 +14,17 @@ export default {
 				sans: ["var(--font-ibm-plex-mono)", ...defaultTheme.fontFamily.sans],
 				serif: ["var(--font-ovo)", ...defaultTheme.fontFamily.serif],
 			},
+            fontSize: {
+                xs: '0.694rem',
+                sm: '0.833rem',
+                base: '1rem',
+                lg: '1.2rem',
+                xl: '1.44rem',
+                '2xl': '1.728rem',
+                '3xl': '2.074rem',
+                '4xl': '2.488rem',
+                '5xl': '2.986rem',
+            },
 		},
 	},
 	plugins: [Forms],
